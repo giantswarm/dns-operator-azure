9cc8df21bf1e81a88daa32373ebbf434870f49db
yulianedyalkova <julianedialkova@gmail.com>
2023-01-09 18:29:11 +0200

    Change check for ParentResourceNotFoundError

--- azure/errors.go
+++ azure/errors.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/azure"
-	capzazure "sigs.k8s.io/cluster-api-provider-azure/azure"
 )
 
 const (
@@ -14,35 +13,7 @@ const (
 
 // ResourceNotFound parses the error to check if it's a resource not found error.
 func IsParentResourceNotFound(err error) bool {
-	if !capzazure.ResourceNotFound(err) {
-		return false
-	}
-
-	detailedErr := autorest.DetailedError{}
-	if !errors.As(err, &detailedErr) {
-		return false
-	}
-
-	azureRequestErrorPtr, ok := detailedErr.Original.(*azure.RequestError)
-	if ok {
-		return azureRequestErrorPtr.ServiceError != nil &&
-			azureRequestErrorPtr.ServiceError.Code == ParentResourceNotFoundErrorCode
-	}
-
-	azureRequestError, ok := detailedErr.Original.(azure.RequestError)
-	if ok {
-		return azureRequestError.ServiceError.Code == ParentResourceNotFoundErrorCode
-	}
-
-	azureServiceErrorPtr, ok := detailedErr.Original.(*azure.ServiceError)
-	if ok {
-		return azureServiceErrorPtr.Code == ParentResourceNotFoundErrorCode
-	}
-
-	azureServiceError, ok := detailedErr.Original.(azure.ServiceError)
-	if ok {
-		return azureServiceError.Code == ParentResourceNotFoundErrorCode
-	}
-
-	return false
+	derr := autorest.DetailedError{}
+	serr := &azure.ServiceError{}
+	return errors.As(err, &derr) && errors.As(derr.Original, &serr) && serr.Code == ParentResourceNotFoundErrorCode
 }
